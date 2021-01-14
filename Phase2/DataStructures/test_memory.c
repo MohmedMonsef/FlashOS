b1afeeabecd7d030c55996cbc8be0762aa2124a8
nadaabdelmaboud <nada5aled52@gmail.com>
2021-01-14 03:40:12 +0200

    ADD Delete Memory - testing

--- Phase2/DataStructures/test_memory.c
+++ Phase2/DataStructures/test_memory.c
@@ -1,18 +1,34 @@
 #include "./memory.h"
-struct Process process[5] = {{1, 3, 29, 8,10},
-                            {1, 3, 29, 8,8},
-                              {2, 13, 8, 9,300},
-                              {9, 43, 24, 3,36},
-                              {10, 49, 8, 10,600}};
+struct Process process[5] = {{1, 3, 29, 8, 10},
+                             {2, 3, 29, 8, 8},
+                             {3, 13, 8, 9, 100},
+                             {9, 43, 24, 3, 36},
+                             {10, 49, 8, 10, 400}};
 int main()
 {
     createMemory();
-        displayMemory();
     for (int i = 0; i < 5; i++)
     {
-        printf("Inser New Process\n");
+        printf("Insert  Process %d\n", i);
         insertInMemory(process[i]);
         displayMemory();
     }
-   
+    for (int i = 0; i < 5; i++)
+    {
+        printf("Delete  Process %d\n", i);
+        deleteFromMemory(process[i]);
+        displayMemory();
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        printf("Insert  Process %d\n", i);
+        insertInMemory(process[i]);
+        displayMemory();
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        printf("Delete  Process %d\n", i);
+        deleteFromMemory(process[i]);
+        displayMemory();
+    }
 }
---
+++ Phase2/Structs/structs.h
@@ -0,0 +1,48 @@
+struct Process
+{
+    int id;
+    int arrival;
+    int runtime;
+    int priority;
+    int memSize;
+};
+struct Node
+{
+    struct Process process;
+    struct Node *next;
+};
+struct LinkedList
+{
+    struct Node *head;
+    struct LinkedList *nextList;
+    struct Node *tail;
+};
+struct PriorityQueue
+{
+    struct LinkedList *front;
+    struct LinkedList *tail;
+    int type;
+};
+/*
+*Messages structs:
+*Note: A message should not contain pointers as it will cause segmentaiton fault. 
+*/
+struct ProcessBuff
+{
+    long header;
+    struct Process content;
+};
+struct RemainingTimeBuff
+{
+    long header;
+    int content;
+};
+
+struct PCB
+{
+    struct Process process;
+    int pid;
+    int totalRunTime;
+    int wait;
+    int lastStopped;
+};
