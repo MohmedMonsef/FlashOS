fc1c9a3662b651f3cd87a2429d2a7438aec12628
MohmedMonsef <mohmedmonsef70@gmail.com>
2021-01-14 16:06:36 +0200

    Implement phase 3 using semaphors

--- Phase3/consumer.c
+++ Phase3/consumer.c
@@ -19,191 +19,141 @@ union Semun
     void *__pad;
 };
 
-struct msgbuff
-{
-    long mtype;
-    char mtext;
-};
-int createQueue(int key);
-int createSem(int key, union Semun *sem);
 int createShmem(int key);
+int createSem(int key, union Semun *sem);
 void up(int sem_id);
 void down(int sem_id);
 void clearResources(int signum);
 /* Producer/consumer program illustrating conditional variables */
 /*keys*/
-#define PRODUCER_Q_KEY 114
-#define CONSUMER_Q_KEY 115
-#define MUTEX_SEM_KEY 202
+#define SEM_FULL_KEY 114
+#define SEM_EMPTY_KEY 115
+#define SEM_MUTEX_KEY 202
 #define SHM_KEY 404
 /*ids*/
-int producer_q_id, consumer_q_id, shm_id, mutex_sem_id;
-union Semun mutex_semun;
+int shm_id, mutex_sem_id, full_sem_id, empty_sem_id;
+union Semun sem_mutex_semun;
+union Semun sem_full_semnum;
+union Semun sem_empty_semun;
 /* Size of shared buffer */
 #define BUF_SIZE 3
 struct Buffer 
 {
     int buffer[BUF_SIZE];							        /* shared buffer */
-    int add;										/* place to add next element */
-    int rem;										/* place to remove next element */
-    int num;										/* number elements in buffer */
+    int next_add;										/* place to next_add next element */
+    int next_rem;										/* place to remove next element */
 };
 struct Buffer *buf;
 struct shmid_ds buff_status;
 
+
 int main()
 {
     signal(SIGINT, clearResources);
-    producer_q_id = createQueue(PRODUCER_Q_KEY);
-    consumer_q_id = createQueue(CONSUMER_Q_KEY);
-    mutex_sem_id = createSem(MUTEX_SEM_KEY, &mutex_semun);
+    /*create semaphore to know if buffer is empty*/
+    empty_sem_id = createSem(SEM_EMPTY_KEY, &sem_empty_semun);
+    /*create semaphore to know if buffer is full*/
+    full_sem_id = createSem(SEM_FULL_KEY, &sem_full_semnum);
+    /*create semaphore to the crtical section*/
+    mutex_sem_id = createSem(SEM_MUTEX_KEY, &sem_mutex_semun);
+    /*create shared memory*/
     shm_id = createShmem(SHM_KEY);
-    struct msgbuff send_message;
-    send_message.mtype = 2;
-    send_message.mtext = 'g';
-    struct msgbuff rec_message;
     int item;
-    char down_again = 0;
-
     while(1)
     {
+        down(full_sem_id);
+        /*critecal section*/
         down(mutex_sem_id);
-
-
-
-        if(buf->num < 0)
-            clearResources(0);
-        /*check if empty*/
-        if(buf->num == 0)
-        {
-            up(mutex_sem_id);
-            down_again = 1;
-            printf("empty\n");
-            //wait for message
-    	    int rec_val = msgrcv(consumer_q_id, &rec_message, sizeof(rec_message.mtext), 0, !IPC_NOWAIT);
-            if (rec_val == -1)
-            {
-                perror("Error in receive");
-                exit(0);
-            }
-        }
-
-        /*down_mutex_sem*/
-        if(down_again == 1)
-        {
-            down(mutex_sem_id);
-            down_again = 0;
-        }
-
-        int rec_val = msgrcv(consumer_q_id, &rec_message, sizeof(rec_message.mtext), 0, IPC_NOWAIT);
-        while (rec_val != -1)
-        {
-            rec_val = msgrcv(consumer_q_id, &rec_message, sizeof(rec_message.mtext), 0, IPC_NOWAIT);
-        }
+        /*consume item*/
+        item = buf->buffer[buf->next_rem];
+        printf("Item removed = %i\n", item);
+        /*incrase the index of the next item to be removed*/
+        buf->next_rem = (buf->next_rem + 1) % BUF_SIZE;
         
-        /*remove item from buffer*/
-        buf->num --;
-        if(buf->num < 0)
-        {
-            buf->num = 0;
-            up(mutex_sem_id);
-            continue;
-        }
-        printf("buf rem = %d\n", buf->rem);
-        item = buf->buffer[buf->rem];
-        printf("Item Consumed = %i\n", item);
-        if(buf->rem == BUF_SIZE - 1)
-            buf->rem = 0;
-        else
-            buf->rem++;
-        
-        printf("Num = %i\n", buf->num);
-        if(buf->num == BUF_SIZE - 1)
-        {
-            printf("Waking the producer up\n");
-            int status = msgsnd(producer_q_id, &send_message, sizeof(send_message.mtext), !IPC_NOWAIT);
-            if (status == -1)
-            {
-                perror("Errror in send\n");
-                exit(0);
-            }
-        }
-        /*up mutex sem*/
         up(mutex_sem_id);
-        //sleep(1);
+        /*critecal section*/
+        up(empty_sem_id);
+        sleep(1);
     }
 }
 
-/* 
- * Return q_id on success and exit on failure.
-*/
-int createQueue(int key)
-{
-    int q_id = msgget(key, 0666 | IPC_CREAT);
-    if (q_id == -1)
-    {
-        perror("Error in creating the msg queue:(\n");
-        exit(-1);
-    }
-    return q_id;
-}
-
+/*creating shared memory*/
 int createShmem(int key)
 {
     int shmid = shmget(key, sizeof(struct Buffer), IPC_CREAT | 0666);
 
     if (shmid == -1)
     {
-        perror("Error in creating the shared memory :(\n");
+        perror("Error in creating the shared memory @ Producer:(\n");
         exit(-1);
     }
 
     buf = shmat(shmid, (void *)0, 0);
     if (buf == (struct Buffer*)-1)
     {
-        perror("Error in attach :(\n");
+        perror("Error in attach @ Producer :(\n");
         exit(-1);
     }
-
+    /*critecal section*/
     down(mutex_sem_id);
-    int status = shmctl(shmid, IPC_STAT, & buff_status);
+    int status = shmctl(shmid, IPC_STAT, &buff_status);
     while (status == -1)
-        status = shmctl(shmid, IPC_STAT, & buff_status);
-
+    {
+        printf("Failed to ge status, try again\n");
+        status = shmctl(shmid, IPC_STAT, &buff_status);
+    }
+    /*if this process is the first to attach then make it initialize the memory*/
     if(buff_status.shm_nattch == 1)
     {
         printf("Initialize the memory\n");
-        buf->num=0;
-        buf->add=0;
-        buf->rem=0;
+        buf->next_add=0;
+        buf->next_rem=0;
     }
     up(mutex_sem_id);
+    /*critecal section*/
     return shmid;
 }
 
 int createSem(int key, union Semun *sem)
 {
     //1. Create Sems:
-    int sem_id = semget(key, 1, 0666 | IPC_CREAT);
+    int sem_id = semget(key, 1, 0666 | IPC_CREAT | IPC_EXCL);
+    if(sem_id == -1)//already exists, just get its id
+    {
+        printf("Just get the id...\n");
+        sem_id = semget(key, 1, 0666 | IPC_CREAT);
+        if (sem_id == -1)
+        {
+            perror("Error in create the semaphor @ Producer :(\n");
+            exit(-1);
+        }
 
-    if (sem_id == -1)
-    {
-        perror("Error in create the semaphor :(\n");
-        exit(-1);
     }
-    /* initial value of the semaphore, Binary semaphore */
-    sem->val = 1;
-    if (semctl(sem_id, 0, SETVAL, *sem) == -1)
+    else //Fist time to be created, set the values.
     {
-        perror("Error in semctl: set value\n");
-        exit(-1);
+        // initial value of the semaphore 
+        switch (key)
+        {
+            case SEM_MUTEX_KEY: printf("sem = mutex"); sem->val = 1; break;
+            case SEM_FULL_KEY : printf("sem = full");sem->val = 0; break;
+            case SEM_EMPTY_KEY: printf("sem = empty");sem->val = BUF_SIZE; break;
+            default: break;
+        }
+        printf("SEM VAL = %i\n",sem->val);
+
+        if (semctl(sem_id, 0, SETVAL, *sem) == -1)
+        {
+            perror("Error in semctl: set value  @ Producer :(\n");
+            exit(-1);
+        }
+
     }
+    //sleep(5);
     return sem_id;
 }
 
 void down(int sem_id)
 {
-    printf("Down\n");
     struct sembuf v_op;
     v_op.sem_num = 0;
     v_op.sem_op = -1;
@@ -213,7 +163,7 @@ void down(int sem_id)
     {
         if (errno != EINTR)
         {
-            perror("Error in down() :(\n");
+            perror("Error in down() @ Producer :(\n");
             exit(-1);
         }
     }
@@ -221,7 +171,6 @@ void down(int sem_id)
 
 void up(int sem_id)
 {
-    printf("Up\n");
     struct sembuf v_op;
 
     v_op.sem_num = 0;
@@ -230,36 +179,32 @@ void up(int sem_id)
 
     if (semop(sem_id, &v_op, 1) == -1)
     {
-        perror("Error in up():(\n");
+        perror("Error in up() @ Producer :(\n");
         exit(-1);
     }
 }
 
 void clearResources(int signum)
 {
+    /*the last process running will clear all resources when interupted*/
     int status = shmctl(shm_id, IPC_STAT, & buff_status);
     while(status == -1)
-    {
-        printf("HAHA\n");
         status = shmctl(shm_id, IPC_STAT, & buff_status);
-    }
-        
-    printf("shm_nattch = %li\n", buff_status.shm_nattch);
 
     // If last process attached, release all resources:
     if(buff_status.shm_nattch < 2)
     {
         printf("Clear resources\n");
         semctl(mutex_sem_id, 0, IPC_RMID, (struct semid_ds *)0);
-        msgctl(producer_q_id, IPC_RMID, (struct msqid_ds *)0);
-        msgctl(consumer_q_id, IPC_RMID, (struct msqid_ds *)0);
+        semctl(full_sem_id, 0, IPC_RMID, (struct semid_ds *)0);
+        semctl(empty_sem_id, 0, IPC_RMID, (struct semid_ds *)0);
         shmctl(shm_id, IPC_RMID, (struct shmid_ds *)0);
     }
-
+    
     // Otherwise, just detach the memory:
     else
     {
-        printf("Detach\n");
+        printf("Detach");
         shmdt(buf);
     }
 
--- Phase3/producer.c
+++ Phase3/producer.c
@@ -19,12 +19,6 @@ union Semun
     void *__pad;
 };
 
-struct msgbuff
-{
-    long mtype;
-    char mtext;
-};
-int createQueue(int key);
 int createShmem(int key);
 int createSem(int key, union Semun *sem);
 void up(int sem_id);
@@ -32,124 +26,58 @@ void down(int sem_id);
 void clearResources(int signum);
 /* Producer/consumer program illustrating conditional variables */
 /*keys*/
-#define PRODUCER_Q_KEY 114
-#define CONSUMER_Q_KEY 115
-#define MUTEX_SEM_KEY 202
+#define SEM_FULL_KEY 114
+#define SEM_EMPTY_KEY 115
+#define SEM_MUTEX_KEY 202
 #define SHM_KEY 404
 /*ids*/
-int producer_q_id, consumer_q_id, shm_id, mutex_sem_id;
-union Semun mutex_semun;
+int shm_id, mutex_sem_id, full_sem_id, empty_sem_id;
+union Semun sem_mutex_semun;
+union Semun sem_full_semnum;
+union Semun sem_empty_semun;
 /* Size of shared buffer */
 #define BUF_SIZE 3
 struct Buffer 
 {
     int buffer[BUF_SIZE];							        /* shared buffer */
-    int add;										/* place to add next element */
-    int rem;										/* place to remove next element */
-    int num;										/* number elements in buffer */
+    int next_add;										/* place to next_add next element */
+    int next_rem;										/* place to remove next element */
 };
 struct Buffer *buf;
-int to_be_added=1;										/*next number to be added*/
 struct shmid_ds buff_status;
 
 
 int main()
 {
     signal(SIGINT, clearResources);
-    producer_q_id = createQueue(PRODUCER_Q_KEY);
-    consumer_q_id = createQueue(CONSUMER_Q_KEY);
-    mutex_sem_id = createSem(MUTEX_SEM_KEY, &mutex_semun);
+    /*create semaphore to know if buffer is empty*/
+    empty_sem_id = createSem(SEM_EMPTY_KEY, &sem_empty_semun);
+    /*create semaphore to know if buffer is full*/
+    full_sem_id = createSem(SEM_FULL_KEY, &sem_full_semnum);
+    /*create semaphore to the crtical section*/
+    mutex_sem_id = createSem(SEM_MUTEX_KEY, &sem_mutex_semun);
+    /*create shared memory*/
     shm_id = createShmem(SHM_KEY);
-    printf("SHM_ID = %i\n", shm_id);
-    int item;
-    struct msgbuff send_message;
-    send_message.mtype = 1;
-    send_message.mtext = 'g'; //go.
-    struct msgbuff rec_message;
-    char down_again = 0;
+    int item = 1;
     while(1)
     {
-        /*produce_item*/
-        item = to_be_added;
+        down(empty_sem_id);
+        /*critical section*/
         down(mutex_sem_id);
-        /*check if full*/
-        if(buf->num == BUF_SIZE)
-        {
-            up(mutex_sem_id);
-            down_again = 1;
-            //wait for message
-            printf("The buffer is full, waiting for the consumer's message... \n");
-    	    int rec_val = msgrcv(producer_q_id, &rec_message, sizeof(rec_message.mtext), 0, !IPC_NOWAIT);
-            if (rec_val == -1)
-            {
-                perror("Error in receive");
-                exit(0);
-            }
-        }
-        /*down_mutex_sem*/
-        if(down_again == 1)
-        {
-            down(mutex_sem_id);
-            down_again = 0;
-        }
 
-        buf->num ++;
-        if(buf->num > BUF_SIZE)
-        {
-            buf->num = BUF_SIZE;
-            up(mutex_sem_id);
-            continue;
-        }
+        buf->buffer[buf->next_add] = item;
+        printf("Item added = %i\n", item);
+        item ++;
+        buf->next_add = (buf->next_add + 1) % BUF_SIZE;
 
-        int rec_val = msgrcv(producer_q_id, &rec_message, sizeof(rec_message.mtext), 0, IPC_NOWAIT);
-        while (rec_val != -1)
-        {
-            rec_val = msgrcv(producer_q_id, &rec_message, sizeof(rec_message.mtext), 0, IPC_NOWAIT);
-        }
-
-        /*add item to buffer*/
-        printf("item produced : %d\n",item);
-        to_be_added++;
-        buf->buffer[buf->add] = item;
-        if(buf->add == BUF_SIZE - 1)
-            buf->add = 0;
-        else
-            buf->add++;
-        printf("Num = %i\n", buf->num);
-        if(buf->num == 1)
-        {
-            printf("Waking the consumer up\n");
-            int status = msgsnd(consumer_q_id, &send_message, sizeof(send_message.mtext), !IPC_NOWAIT);
-            if (status == -1)
-            {
-                perror("Errror in send: Invalid argument\n");
-                exit(0);
-            }
-        }
-
-        /*up mutex sem*/
         up(mutex_sem_id);
-        //sleep(1);
+        /*critical section*/
+        up(full_sem_id);
+        sleep(1);
     }
 }
 
-
-/* 
- * Return q_id on success and exit on failure.
-*/
-int createQueue(int key)
-{
-    int q_id = msgget(key, 0666 | IPC_CREAT);
-    if (q_id == -1)
-    {
-        perror("Error in creating the msg queue @ Producer :(\n");
-        exit(-1);
-    }
-
-    printf("Done Q creation in Producer\n");
-    return q_id;
-}
-
+/*creating shared memory*/
 int createShmem(int key)
 {
     int shmid = shmget(key, sizeof(struct Buffer), IPC_CREAT | 0666);
@@ -166,7 +94,7 @@ int createShmem(int key)
         perror("Error in attach @ Producer :(\n");
         exit(-1);
     }
-
+    /*critecal section*/
     down(mutex_sem_id);
     int status = shmctl(shmid, IPC_STAT, &buff_status);
     while (status == -1)
@@ -174,45 +102,59 @@ int createShmem(int key)
         printf("Failed to ge status, try again\n");
         status = shmctl(shmid, IPC_STAT, &buff_status);
     }
-
+    /*if this process is the first to attach then make it initialize the memory*/
     if(buff_status.shm_nattch == 1)
     {
         printf("Initialize the memory\n");
-        buf->num=0;
-        buf->add=0;
-        buf->rem=0;
+        buf->next_add=0;
+        buf->next_rem=0;
     }
     up(mutex_sem_id);
-
-    printf("Done mem creation @ producer\n");
+    /*critecal section*/
     return shmid;
 }
 
 int createSem(int key, union Semun *sem)
 {
     //1. Create Sems:
-    int sem_id = semget(key, 1, 0666 | IPC_CREAT);
-
-    if (sem_id == -1)
+    int sem_id = semget(key, 1, 0666 | IPC_CREAT | IPC_EXCL);
+    if(sem_id == -1)//already exists, just get its id
     {
-        perror("Error in create the semaphor @ Producer :(\n");
-        exit(-1);
-    }
-    /* initial value of the semaphore, Binary semaphore */
-    sem->val = 1;
-    if (semctl(sem_id, 0, SETVAL, *sem) == -1)
-    {
-        perror("Error in semctl: set value  @ Producer :(\n");
-        exit(-1);
-    }
+        printf("Just get the id...\n");
+        sem_id = semget(key, 1, 0666 | IPC_CREAT);
+        if (sem_id == -1)
+        {
+            perror("Error in create the semaphor @ Producer :(\n");
+            exit(-1);
+        }
 
-    printf("Done sem creation @ producer\n");
+    }
+    else //Fist time to be created, set the values.
+    {
+        // initial value of the semaphore 
+        switch (key)
+        {
+            case SEM_MUTEX_KEY: printf("sem = mutex"); sem->val = 1; break;
+            case SEM_FULL_KEY : printf("sem = full");sem->val = 0; break;
+            case SEM_EMPTY_KEY: printf("sem = empty");sem->val = BUF_SIZE; break;
+            default: break;
+        }
+        printf("SEM VAL = %i\n",sem->val);
+
+        if (semctl(sem_id, 0, SETVAL, *sem) == -1)
+        {
+            perror("Error in semctl: set value  @ Producer :(\n");
+            exit(-1);
+        }
+
+    }
+    
+    //sleep(5);
     return sem_id;
 }
 
 void down(int sem_id)
 {
-    printf("Down\n");
     struct sembuf v_op;
     v_op.sem_num = 0;
     v_op.sem_op = -1;
@@ -230,7 +172,6 @@ void down(int sem_id)
 
 void up(int sem_id)
 {
-    printf("Up\n");
     struct sembuf v_op;
 
     v_op.sem_num = 0;
@@ -246,32 +187,25 @@ void up(int sem_id)
 
 void clearResources(int signum)
 {
-    struct shmid_ds buff_status;
+    /*the last process running will clear all resources when interupted*/
     int status = shmctl(shm_id, IPC_STAT, & buff_status);
-    printf("Shm_id = % i\n", shm_id);
     while(status == -1)
-    {
-        //printf("HAHA\n");
         status = shmctl(shm_id, IPC_STAT, & buff_status);
-    }
-        
-    printf("shm_nattch = %li\n", buff_status.shm_nattch);
 
     // If last process attached, release all resources:
     if(buff_status.shm_nattch < 2)
     {
         printf("Clear resources\n");
         semctl(mutex_sem_id, 0, IPC_RMID, (struct semid_ds *)0);
-        msgctl(producer_q_id, IPC_RMID, (struct msqid_ds *)0);
-        msgctl(consumer_q_id, IPC_RMID, (struct msqid_ds *)0);
+        semctl(full_sem_id, 0, IPC_RMID, (struct semid_ds *)0);
+        semctl(empty_sem_id, 0, IPC_RMID, (struct semid_ds *)0);
         shmctl(shm_id, IPC_RMID, (struct shmid_ds *)0);
     }
     
-
     // Otherwise, just detach the memory:
     else
     {
-        printf("Detach\n");
+        printf("Detach");
         shmdt(buf);
     }
 
